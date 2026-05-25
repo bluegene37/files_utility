fd47b25c9ff489cddd410cda2654d10eccecd296
bluegene37 <bluegene37@outlook.com>
2026-05-25 15:02:10 +0800

    chore: rename project from email_attachment_cleaner to files_utility across all platform configurations and package settings

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:email_attachment_cleaner/main.dart';
+import 'package:files_utility/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
